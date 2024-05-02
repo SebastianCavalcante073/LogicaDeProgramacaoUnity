8e9ba5e6abc0a9016ad7782dcc82f5a32ef807ee
SebastianCavalcante <sebastiancavalcante073@outlook.com>
2024-05-01 23:19:57 -0300

    Tipos de Variáveis

--- PrimeiroScriptCsharp.cs
+++ PrimeiroScriptCsharp.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class PrimeiroScriptCsharp : MonoBehaviour
 {
+    // Exemplos de Variáveis Básicas C#
+    char Sexo = 'M';
+    int Idade = 18;
+    float Altura = 1.75f;
+    double Saldo = 500.0;
+    bool Adulto = false;
+
+    // Exemplo de Variáveis Unity
+    GameObject Mochila;
+    Rigidbody ComponenteDeFisica;
+    Collider ComponenteDeColisao;
+    
     // Start é chamado um vez ao iniciar o jogo
     void Start()
     {

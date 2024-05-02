2188b0f50f233ecdee6b6fc4b184c6e654421790
SebastianCavalcante <sebastiancavalcante073@outlook.com>
2024-05-01 23:46:05 -0300

    Estrutura de Repetição For

---
+++ EstruturaFor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstruturaFor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Estrutura de Repetição for
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log("Estrutura de repetição for ");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

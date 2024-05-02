f77d40a3864ebf87fd40f208dbde02d9b49800e9
SebastianCavalcante <sebastiancavalcante073@outlook.com>
2024-05-01 22:54:18 -0300

    Criando o primeiro script c#

---
+++ PrimeiroScriptCsharp.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimeiroScriptCsharp : MonoBehaviour
+{
+    // Start é chamado um vez ao iniciar o jogo
+    void Start()
+    {
+        
+    }
+
+    // Update é chamado a cada frame do jogo
+    void Update()
+    {
+        
+    }
+}
